0d89e402bf109ac59bae7bff5f98dc3cfb003f2a
Shaan Khosla <35707672+shaankhosla@users.noreply.github.com>
2025-12-17 15:38:23 -0700

    repeat 0.0.3
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/repeat.rb
+++ Formula/repeat.rb
@@ -1,9 +1,9 @@
 class Repeat < Formula
   desc "Your tool description"
   homepage "https://github.com/shaankhosla/repeat"
-  url "https://github.com/shaankhosla/repeat/releases/download/v0.0.1/repeat"
-  sha256 "SOME_SHA256"
-  version "0.0.1"
+  url "https://github.com/shaankhosla/repeat/releases/download/v0.0.3/repeat"
+  sha256 "90ec9d0d1ad279674aedf146e48730f7fddb88d1ac4582060c8280fba79f8f5d"
+  version "0.0.3"
 
   def install
     bin.install "repeat"
