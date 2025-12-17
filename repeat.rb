3dbd159b9ec58bb1ba0b743797bbf70821547bc9
Shaan <shaankhosla@macbook-pro.mynetworksettings.com>
2025-12-17 15:08:43 -0700

    init repeat

---
+++ repeat.rb
@@ -0,0 +1,11 @@
+class Repeat < Formula
+  desc "Your tool description"
+  homepage "https://github.com/shaankhosla/repeat"
+  url "https://github.com/shaankhosla/repeat/releases/download/v0.0.1/repeat"
+  sha256 "SOME_SHA256"
+  version "0.0.1"
+
+  def install
+    bin.install "repeat"
+  end
+end
