0481c7e2ea7a1f9b6194f7fd8576b8549c7e1652
Shaan Khosla <35707672+shaankhosla@users.noreply.github.com>
2025-12-23 13:26:02 -0700

    repeat 0.0.9
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/repeat.rb
+++ Formula/repeat.rb
@@ -1,9 +1,9 @@
 class Repeat < Formula
   desc "Your tool description"
   homepage "https://github.com/shaankhosla/repeat"
-  url "https://github.com/shaankhosla/repeat/releases/download/v0.0.8/repeat-darwin-arm64.tar.gz"
-  sha256 "505a6924beda0a8cfd11bcaa2f3ee6fcbdf6908d2a58f69fe6577e1dea3ac5d3"
-  version "0.0.8"
+  url "https://github.com/shaankhosla/repeat/releases/download/v0.0.9/repeat-darwin-arm64.tar.gz"
+  sha256 "6277902462654622920852cf688bc5b6ccbd51bdc01a284c83da3cb758303b07"
+  version "0.0.9"
 
   def install
     bin.install "repeat"
