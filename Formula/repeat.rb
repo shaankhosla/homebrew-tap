20667b518eab74fd4ea0ad75b265a4e236561872
Shaan Khosla <35707672+shaankhosla@users.noreply.github.com>
2025-12-24 10:16:29 -0700

    repeat 0.0.11
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/repeat.rb
+++ Formula/repeat.rb
@@ -1,9 +1,9 @@
 class Repeat < Formula
   desc "Your tool description"
   homepage "https://github.com/shaankhosla/repeat"
-  url "https://github.com/shaankhosla/repeat/releases/download/v0.0.10/repeat-v0.0.10-aarch64-apple-darwin.tar.gz"
-  sha256 "9eb9d43e8927997b2243ef075561a60d19930e4c5e99e2b84ac3e092ef69cadc"
-  version "0.0.10"
+  url "https://github.com/shaankhosla/repeat/releases/download/v0.0.11/repeat-v0.0.11-aarch64-apple-darwin.tar.gz"
+  sha256 "47b28f0d9ffb93f234be0904ff56453540d2e91d1cf1c36f8187c17cbd2ed96b"
+  version "0.0.11"
 
   def install
     bin.install "repeat"
